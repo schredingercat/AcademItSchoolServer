aedebfb19e67b05ec7ea7734e34bb34f33e619cd
Ilya Bogomolov <bogomolov_comfortlab@bitbucket.org>
2019-06-19 14:08:03 +0700

    Создан проект ConditionalCompilation

---
+++ AcademItSchoolServer/ConditionalCompilation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConditionalCompilation
+{
+    class Program
+    {
+        static void Main()
+        {
+            #region ConditionalCompilationRegion
+#if (DEBUG)
+            Console.WriteLine("Debug");
+#else
+            Console.WriteLine("Release");
+#endif
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
