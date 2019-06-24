77d67a452112fc811281d68c30ec8092e3b2adb2
Ilya Bogomolov <bogomolov_comfortlab@bitbucket.org>
2019-06-24 15:54:36 +0700

    Добавлен проект LogOut

---
+++ AcademItSchoolServer/LogOut/WriteToLog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LogOut
+{
+    class WriteToLog
+    {
+        static void Main()
+        {
+            Log.Debug("Hello");
+            Log.Debug("World");
+            Log.Debug("!");
+
+            Log.Debug("Вывод исключения");
+            try
+            {
+                var zero = 1 - 1;
+                Console.WriteLine(1 / zero);
+            }
+            catch (Exception ex)
+            {
+                Log.Debug(ex);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    }
+}
