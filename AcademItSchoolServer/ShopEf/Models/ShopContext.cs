f6dee1d7f247f074e2eeb6157c620ed77e642ca2
Ilya Bogomolov <bogomolov_comfortlab@bitbucket.org>
2019-07-25 13:27:41 +0700

    Добавлен проект ShopEf

---
+++ AcademItSchoolServer/ShopEf/Models/ShopContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopEf.Models
+{
+    public class ShopContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public ShopContext() : base("DefaultConnection")
+        {
+
+        }
+
+    }
+}
---
+++ AcademItSchoolServer/ShopEf/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopEf.Models;
+
+namespace ShopEf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var dB = new ShopContext())
+            {
+                Console.WriteLine(dB.Categories.Count());
+            }
+
+            Console.ReadLine();
+        }
+        
+    }
+}
